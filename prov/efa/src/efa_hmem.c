27380c7634361a1ba683b7f0588f7397f6b558d5
Mitchell Young <mthyoung@amazon.com>
2026-09-17 18:59:55 +0000

    efa/hmem: Only create CUDA ctx for probe if necessary
    Before, we would unconditionally push and pop a CUDA context to service
    the p2p probe allocation. Now, we check if there is already a CUDA
    context and use it if there is. Benefits include avoiding ctx
    management overhead, and ensuring that we use the device that the
    application will use, in the case that it's already claimed one.
    
    After testing, will split this into two commits: one for the new CUDA
    API wrapper and another with the efa code change

--- prov/efa/src/efa_hmem.c
+++ prov/efa/src/efa_hmem.c
@@ -118,7 +118,7 @@ static int efa_hmem_info_init_protocol_thresholds(enum fi_hmem_iface iface)
 static inline void efa_hmem_info_check_p2p_support_cuda(struct efa_hmem_info *info) {
 #if HAVE_CUDA
 	CUdevice cu_dev;
-	CUcontext cu_ctx;
+	CUcontext cu_ctx = NULL;
 	CUresult cu_ret;
 	CUdeviceptr ptr = 0;
 	struct ibv_mr *ibv_mr;
@@ -128,32 +128,46 @@ static inline void efa_hmem_info_check_p2p_support_cuda(struct efa_hmem_info *in
 	int ret;
 	int dmabuf_fd;
 	uint64_t dmabuf_offset;
+	bool own_cuda_ctx = false;
 
-	cu_ret = ofi_cuDeviceGet(&cu_dev, 0);
+	cu_ret = ofi_cuCtxGetCurrent(&cu_ctx);
 	if (cu_ret != CUDA_SUCCESS) {
 		info->initialized = false;
-		EFA_WARN(FI_LOG_CORE, "Failed to get CUDA device: %d\n", cu_ret);
+		EFA_WARN(FI_LOG_CORE, "Failed to probe CUDA ctx: %d\n", cu_ret);
 		return;
 	}
 
-	/*
-	 * Create a CUDA context explicitly. This is to prevent implicit ctx creation when
-	 * using the higher-level CUDA runtime functions (e.g., cudaMalloc). Managing the
-	 * ctx ourselves lets us make sure that we clean up properly to have minimal impact
-	 * on the system after we are done probing for p2p support.
-	*/
-	cu_ret = ofi_cuCtxCreate_v2(&cu_ctx, 0, cu_dev);
-	if (cu_ret != CUDA_SUCCESS) {
-		info->initialized = false;
-		EFA_WARN(FI_LOG_CORE, "Failed to create CUDA context: %d\n", cu_ret);
-		return;
+	if (!cu_ctx) {
+		cu_ret = ofi_cuDeviceGet(&cu_dev, 0);
+		if (cu_ret != CUDA_SUCCESS) {
+			info->initialized = false;
+			EFA_WARN(FI_LOG_CORE, "Failed to get CUDA device: %d\n", cu_ret);
+			return;
+		}
+
+		/*
+		 * Create a CUDA context explicitly. This is to prevent implicit ctx creation when
+		 * using the higher-level CUDA runtime functions (e.g., cudaMalloc). Managing the
+		 * ctx ourselves lets us make sure that we clean up properly to have minimal impact
+		 * on the system after we are done probing for p2p support.
+		 */
+		cu_ret = ofi_cuCtxCreate_v2(&cu_ctx, 0, cu_dev);
+		if (cu_ret != CUDA_SUCCESS) {
+			info->initialized = false;
+			EFA_WARN(FI_LOG_CORE, "Failed to create CUDA context: %d\n", cu_ret);
+			return;
+		}
+
+		own_cuda_ctx = true;
 	}
 
 	cu_ret = ofi_cuMemAlloc(&ptr, len);
 	if (cu_ret != CUDA_SUCCESS) {
 		info->initialized = false;
 		EFA_WARN(FI_LOG_CORE, "Failed to allocate CUDA buffer: %d\n", cu_ret);
-		ofi_cuCtxDestroy(cu_ctx);
+		if (own_cuda_ctx) {
+			ofi_cuCtxDestroy(cu_ctx);
+		}
 		return;
 	}
 
@@ -161,7 +175,9 @@ static inline void efa_hmem_info_check_p2p_support_cuda(struct efa_hmem_info *in
 	if (!ibv_pd) {
 		EFA_WARN(FI_LOG_CORE, "failed to allocate ibv_pd: %d\n", errno);
 		ofi_cuMemFree(ptr);
-		ofi_cuCtxDestroy(cu_ctx);
+		if (own_cuda_ctx) {
+			ofi_cuCtxDestroy(cu_ctx);
+		}
 		return;
 	}
 
@@ -203,7 +219,9 @@ static inline void efa_hmem_info_check_p2p_support_cuda(struct efa_hmem_info *in
 			 "Failed to register CUDA buffer with the EFA device, FI_HMEM transfers that require peer to peer support will fail.\n");
 		ofi_cuMemFree(ptr);
 		(void) ibv_dealloc_pd(ibv_pd);
-		ofi_cuCtxDestroy(cu_ctx);
+		if (own_cuda_ctx) {
+			ofi_cuCtxDestroy(cu_ctx);
+		}
 		return;
 	}
 
@@ -214,13 +232,16 @@ static inline void efa_hmem_info_check_p2p_support_cuda(struct efa_hmem_info *in
 		EFA_WARN(FI_LOG_CORE,
 			 "Failed to deregister CUDA buffer: %s\n",
 			 fi_strerror(-ret));
-		ofi_cuCtxDestroy(cu_ctx);
+		if (own_cuda_ctx) {
+			ofi_cuCtxDestroy(cu_ctx);
+		}
 		return;
 	}
 
 	info->p2p_supported_by_device = true;
-	ofi_cuCtxDestroy(cu_ctx);
-	return;
+	if (own_cuda_ctx) {
+		ofi_cuCtxDestroy(cu_ctx);
+	}
 
 #endif
 	return;
